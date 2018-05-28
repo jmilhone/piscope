520b608bcccb19f932d773d17af216450714556f
Milhone <jmilhone@wisc.edu>
2018-05-28 09:21:04 -0500

    Add lru_cache functionality to grabbing data
    Created a helper function called _retrieve_signal where all inputs are
    hashable so that @functools.lru_cache could be utilized.
    
    retrieve_signal now unpacks some of the inputs and calls
    _retrieve_signal.
    
    Some of the logging messages were modified as well.

--- app/data/mdsplus_helpers.py
+++ app/data/mdsplus_helpers.py
@@ -3,6 +3,7 @@ import MDSplus as mds
 from .data import Data
 from ..logging.piscope_logging import log, time_log
 import logging
+from functools import lru_cache
 
 logger = logging.getLogger('pi-scope-logger')
 
@@ -14,13 +15,13 @@ def get_current_shot(server, tree):
         current_shot = int(con.get("$SHOT"))
         return current_shot
     except mds.mdsExceptions.TreeNOCURRENT as e:
-        logger.warn('TreeNOCURRENT in get_current_shot')
+        logger.warning('TreeNOCURRENT in get_current_shot')
         return
     except (mds.MdsIpException, mds.TreeFOPENR, mds.TdiMISS_ARG) as e:
-        logger.warn('Random MDSplus error in get_current_shot')
+        logger.warning('Random MDSplus error in get_current_shot')
         return
     except ValueError as e:
-        logger.warn('ValueError in get_current_shot')
+        logger.warning('ValueError in get_current_shot')
         return
 
 
@@ -34,6 +35,7 @@ def check_data_dictionary(data_dict):
     # If you make it to here, that means all items were None
     return False
 
+
 @log(logger)
 def check_open_tree(shot_number, server, tree):
     try:
@@ -41,42 +43,80 @@ def check_open_tree(shot_number, server, tree):
         con.openTree(tree, shot_number)
         return True
     except (mds.MdsIpException, mds.TreeFOPENR, mds.TdiMISS_ARG) as e:
-        logger.warn('Error opening shot %d' % shot_number)
+        logger.warning('Error opening shot %d' % shot_number)
         # print("Error with shot {0:d}".format(shot_number))
         # print(e.message)
         return False
 
 
+# @log(logger)
+# def retrieve_signal(shot_number, signal_info, loc_name, signal_name, server, tree):
+
+    # try:
+    #     con = mds.Connection(server)
+    #     con.openTree(tree, shot_number)
+    #     logger.debug("Retrieving data for %s" % signal_name)
+    #     data = retrieve_data(con, signal_info, signal_name)
+    # except (mds.MdsIpException, mds.TreeFOPENR, mds.TdiMISS_ARG) as e:
+    #     logger.warn('Random MDSplus error in retrieve_signal')
+    #     # print("Error with shot {0:d}, loc {1}, name {2}".format(shot_number, loc_name, signal_name))
+    #     # print(e.message)
+    #     data = None
+    # return loc_name, signal_name, data
+
+
 @log(logger)
 def retrieve_signal(shot_number, signal_info, loc_name, signal_name, server, tree):
+    xstring = signal_info['x']
+    ystring = signal_info['y']
+    color = signal_info['color']
 
+    data = _retrieve_signal(shot_number, server, tree, xstring, ystring,
+                           signal_name, color)
+
+    return loc_name, signal_name, data
+
+    # try:
+    #     con = mds.Connection(server)
+    #     con.openTree(tree, shot_number)
+    #     logger.debug("Retrieving data for %s" % signal_name)
+    #     data = retrieve_data(con, signal_info, signal_name)
+    # except (mds.MdsIpException, mds.TreeFOPENR, mds.TdiMISS_ARG) as e:
+    #     logger.warn('Random MDSplus error in retrieve_signal')
+    #     # print("Error with shot {0:d}, loc {1}, name {2}".format(shot_number, loc_name, signal_name))
+    #     # print(e.message)
+    #     data = None
+    # return loc_name, signal_name, data
+
+
+@lru_cache(maxsize=512)
+def _retrieve_signal(shot_number, server, tree, xstring, ystring, name, color):
     try:
         con = mds.Connection(server)
         con.openTree(tree, shot_number)
-        logger.debug("Retrieving data for %s" % signal_name)
-        data = retrieve_data(con, signal_info, signal_name)
+        logger.debug("Retrieving data for %s" % name)
+        data = retrieve_data(con, xstring, ystring, name, color)
+        return data
+
     except (mds.MdsIpException, mds.TreeFOPENR, mds.TdiMISS_ARG) as e:
-        logger.warn('Random MDSplus error in retrieve_signal')
-        # print("Error with shot {0:d}, loc {1}, name {2}".format(shot_number, loc_name, signal_name))
-        # print(e.message)
-        data = None
-    return loc_name, signal_name, data
+        logger.warning('Random MDSplus error in retrieve_signal')
+        return
 
 
 @time_log(logger)
-def retrieve_data(connection, node_loc, name):
+def retrieve_data(connection, xstr, ystr, name, color):
     try:
-        if "\n" in node_loc['y']:
-            ystring = node_loc['y'].splitlines()
+        if "\n" in ystr:
+            ystring = ystr.splitlines()
             ystring = " ".join(ystring)
         else:
-            ystring = node_loc['y']
+            ystring = ystr
 
-        if "\n" in node_loc['x']:
-            xstring = node_loc['x'].splitlines()
+        if "\n" in xstr:
+            xstring = xstr.splitlines()
             xstring = " ".join(xstring)
         else:
-            xstring = node_loc['x']
+            xstring = xstr
 
         data = connection.get(ystring)
         t = connection.get(xstring)
@@ -88,16 +128,54 @@ def retrieve_data(connection, node_loc, name):
         data = data.data()
         t = t.data()
 
-        return Data(name, t, data, node_loc['color'])
+        return Data(name, t, data, color)
 
     except mds.MdsIpException:
-        logger.warn('MdsIPException occurred in retrieve_data for %s' % name)
+        logger.warning('MdsIPException occurred in retrieve_data for %s' % name)
         return
-    except mds.TreeNODATA as e:
-        logger.warn('TreeNODATA occurred in retrieve_data for %s' % name)
+    except mds.TreeNODATA:
+        logger.warning('TreeNODATA occurred in retrieve_data for %s' % name)
         return
     except KeyError:
-        logger.warn('KeyError occured in retrieve_data for %s' % name)
+        logger.warning('KeyError occured in retrieve_data for %s' % name)
         return
+
+# @time_log(logger)
+# def retrieve_data(connection, node_loc, name):
+#     try:
+#         if "\n" in node_loc['y']:
+#             ystring = node_loc['y'].splitlines()
+#             ystring = " ".join(ystring)
+#         else:
+#             ystring = node_loc['y']
+
+        # if "\n" in node_loc['x']:
+        #     xstring = node_loc['x'].splitlines()
+        #     xstring = " ".join(xstring)
+
+        #     xstring = node_loc['x']
+
+        # data = connection.get(ystring)
+        # t = connection.get(xstring)
+
+        # # apparently you can get None without any errors
+        # if data is None or t is None:
+        #     return None
+
+        # data = data.data()
+        # t = t.data()
+
+        # return Data(name, t, data, node_loc['color'])
+
+    # except mds.MdsIpException:
+    #     logger.warn('MdsIPException occurred in retrieve_data for %s' % name)
+    #     return
+    # except mds.TreeNODATA as e:
+    #     logger.warn('TreeNODATA occurred in retrieve_data for %s' % name)
+    #     return
+    # except KeyError:
+    #     logger.warn('KeyError occured in retrieve_data for %s' % name)
+    #     return
+
 
 
